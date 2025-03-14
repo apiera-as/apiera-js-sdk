7bbf02031f1b19a6a6c28957b839d134417b6599
fredrik-tveraaen <fredrik.tveraaen@apiera.io>
2025-03-14 15:37:58 +0100

    Remove generated files from Git tracking

--- src/dto/base/AbstractDTO.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AbstractDTO = void 0;
-/**
- * Abstract base class for all DTOs
- */
-class AbstractDTO {
-}
-exports.AbstractDTO = AbstractDTO;
--- src/enum/IntegrationStatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IntegrationStatus = void 0;
-// IntegrationStatus.ts
-var IntegrationStatus;
-(function (IntegrationStatus) {
-    IntegrationStatus["ACTIVE"] = "active";
-    IntegrationStatus["INACTIVE"] = "inactive";
-})(IntegrationStatus || (exports.IntegrationStatus = IntegrationStatus = {}));
